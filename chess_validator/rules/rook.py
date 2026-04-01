d34435f1542b83d16cf777747ab1609c198475f8
rtbroadhurst <rtbroadhurst@gmail.com>
2026-04-01 23:35:08 +0100

    Add basic rook rules and tests

---
+++ chess_validator/rules/rook.py
@@ -0,0 +1,43 @@
+"""Contains piece specific rules for the rook"""
+
+from ..utilities import get_delta
+
+
+def is_valid_rook_move(board, start_row, start_col, end_row, end_col) -> bool:
+    """Return True if valid rook ove, otherwise False"""
+    
+    # Get move deltas
+    delta_row, delta_col = get_delta(start_row, start_col, end_row, end_col)
+    
+    
+    # Move must end in the same row or same column
+    if delta_row and delta_col:
+        return False
+    
+    
+    # Move must not be blocked by an intermediate square
+    if delta_row > 0:
+        step_row = 1
+    elif delta_row < 0:
+        step_row = -1
+    else:
+        step_row = 0
+        
+    if delta_col > 0:
+        step_col = 1
+    elif delta_col < 0:
+        step_col = -1
+    else:
+        step_col = 0
+      
+    current_row = start_row + step_row
+    current_col = start_col + step_col
+
+    while current_row != end_row or current_col != end_col:
+        if not board.is_empty(current_row, current_col):
+            return False
+        current_row += step_row
+        current_col += step_col
+
+    return True
+    
