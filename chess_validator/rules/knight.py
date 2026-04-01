772166a4f12d8f5b1477903a213bdfd1f94129c2
rtbroadhurst <rtbroadhurst@gmail.com>
2026-04-02 00:48:51 +0100

    Add movement logic for knight

--- chess_validator/rules/knight.py
+++ chess_validator/rules/knight.py
@@ -1,7 +1,16 @@
-"""Contains piece specific rules for the bishop"""
+"""Contains piece specific rules for the knight"""
 
 from ..utilities import get_delta
 
 
-def is_valid_bishop_move(board, start_row, start_col, end_row, end_col) -> bool:
-    """Return True if valid bishop move, otherwise False"""
+def is_valid_knight_move(board, start_row, start_col, end_row, end_col) -> bool:
+    """Return True if valid knight move, otherwise False"""
+    
+    # Get move delta
+    delta_row, delta_col = get_delta(start_row, start_col, end_row, end_col)
+
+    # Move must be in L shape
+    if (abs(delta_row), abs(delta_col)) not in {(2, 1), (1, 2)}:
+        return False
+
+    return True
